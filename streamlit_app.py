1c40af4f1b9c77bd65787d82193141015134aaee
GGozdecki <75222971+GGozdecki@users.noreply.github.com>
2022-05-30 00:33:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My first Streamlit app')
 streamlit.header('To do list:')
-streamlit.text('- 8:00 Wake up!')
-streamlit.text('- 10:00 Start work')
-streamlit.text('- 15:00 Dinner')
-streamlit.text('- 18:00 End work')
-streamlit.text('- 22:30 Go to bed')
+streamlit.text('⏰ 8:00 Wake up!')
+streamlit.text('💻 10:00 Start work')
+streamlit.text('🍱 15:00 Dinner')
+streamlit.text('🔌 18:00 End work')
+streamlit.text('🛌 22:30 Go to bed')
+

356cc8d8892f32a8f0bb758e288c296996c09ec9
GGozdecki <75222971+GGozdecki@users.noreply.github.com>
2022-05-30 00:24:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 
 streamlit.title('My first Streamlit app')
 streamlit.header('To do list:')
-streamlit.markdown('- 8:00 Wake up! - 10:00 Start work')
+streamlit.markdown('- 8:00 Wake up! 
+                   - 10:00 Start work')
